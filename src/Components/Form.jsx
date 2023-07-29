4daf486ef5ffda0d814d8b2a6b6239ea7b0794ba
Sadzida <sadzidadziho@gmail.com>
2023-07-29 19:44:07 +0200

    Added Home button

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -7,7 +7,9 @@ function Form ()
     
     return ( <form className='Forma'>
     <label id='Name'> Sadžida Džiho</label>
-    <div className="Buttons"><button className="btn1" ><Link className="Edu" to="/Education">Education</Link></button>
+    <div className="Buttons">
+    <button className="btnHome"><Link className="Edu" to="/">Home</Link></button>
+    <button className="btn1" ><Link className="Edu" to="/Education">Education</Link></button>
     <button className="btn2">btn2</button>
     <button className="btn3">btn3</button></div>
     </form>)
