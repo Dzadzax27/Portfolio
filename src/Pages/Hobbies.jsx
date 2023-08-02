56bb38f574ff8f6dd48c4f163cd6d76fd6768326
Sadzida <sadzidadziho@gmail.com>
2023-08-02 11:29:24 +0200

    Edited design of a page

--- src/Pages/Hobbies.jsx
+++ src/Pages/Hobbies.jsx
@@ -7,8 +7,12 @@ function Hobbies()
     return <div>
     <Form></Form>    
     <div className='Main'></div>
-
-
+    <div className='Hobbies'>
+    <div className='Sport'><div className='Title'>Sport</div>
+    <br></br></div>
+    <div className='volunteerism'><div className='Title'>Volunteerism</div></div>
+    <div className='Awards'><div className='Title'>Awards</div></div>
+    </div>
     </div>
 }
 export default Hobbies;
