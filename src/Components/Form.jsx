4a1db1b4d7a926293a8a4170ede2b72131bad63e
Sadzida <sadzidadziho@gmail.com>
2023-08-10 12:02:19 +0200

    Fixed error with resizing

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -5,20 +5,20 @@ import React,{ useState } from 'react';
 function Form ()
 {
   
-    const [menuOpen, setMenuOpen] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(true);
     const toggleMenu = () => {
           setMenuOpen(!menuOpen);
         };
-    return ( <div className="Navbar"><button className={`Toggle ${menuOpen ? '' : 'active'}`}  onClick={toggleMenu}>
+    return ( <div className="Navbar"><button className="Toggle"  onClick={toggleMenu}>
     <img className="Menu" src="https://cdn-icons-png.flaticon.com/512/56/56763.png"></img>
     </button><form className="Forma">
     <label id='Name'> Sadžida Džiho</label>
     </form>
     <div className={`Buttons ${menuOpen ? 'active' : ''}`} >
-    <Link className="Edu" to="/"><button className="btnHome">Home</button></Link>
-    <Link className="Edu" to="/Education"><button className="btn1" >Education</button></Link>
-    <Link className="Edu" to="/Skills"><button className="btn2">Skills</button></Link>
-    <Link className="Edu" to="/Hobbies"><button className="btn3">Hobbies</button></Link></div>
+    <Link onClick={toggleMenu} className="Edu" to="/"><button  className="btnHome">Home</button></Link>
+    <Link className="Edu" to="/Education" onClick={toggleMenu}><button  className="btn1" >Education</button></Link>
+    <Link className="Edu" to="/Skills"><button onClick={toggleMenu} className="btn2">Skills</button></Link>
+    <Link className="Edu" to="/Hobbies"><button onClick={toggleMenu}  className="btn3">Hobbies</button></Link></div>
     
     </div>)
 }
